8aa09ff10271a4361c8d11ad6d7013ea65ae2c1a
HakeshK <40683105+HakeshKumar@users.noreply.github.com>
2026-09-15 14:35:56 -0500

    datalemur: Second Day Confirmation TikTok [PostgreSQL 14]

--- datalemur/second-day-confirmation/solution.sql
+++ datalemur/second-day-confirmation/solution.sql
@@ -3,4 +3,4 @@ FROM emails
 INNER JOIN texts
   ON emails.email_id = texts.email_id
 WHERE texts.action_date = emails.signup_date + INTERVAL '1 day'
-  AND texts.signup_action = 'Confirm
+  AND texts.signup_action = 'Confirmed';
